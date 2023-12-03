a4ddbf0a8129307f3cde0f22dc9facbeca83507a
Panoramix de Druïde <panoramix@xn--dru-1ma.de>
2023-12-03 23:57:01 +0100

    Done for now, gonna try to add support for longer numbers later

--- calculator.h
+++ calculator.h
@@ -1,10 +1,14 @@
 #define A 420
 #define B 69
+DOING MATHS WITH A AND B...
+
+A TIMES B EQUALS
 #include "getdigit.h"
 #include "multiplier.h"
-/*
 ??=include "clear.h"
+A PLUS B EQUALS
 #include "adder.h"
 ??=include "clear.h"
+A MINUS B EQUALS
 #include "subtractor.h"
-*/
+
