f153348d3db33654284da847fe4326b734d5c3fd
Robert Rinia <robert@rinia.tv>
2023-12-04 05:08:07 +0100

    Code broke, oops

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@
 if [[ ! -f processed_numbers.h ]]; then
 	cpp -P -Wno-trigraphs -I number_sources number_sources/numbers.h -o processed_numbers.h
 fi
-#cpp -P -Wno-trigraphs -trigraphs calculator.h
+cpp -P -Wno-trigraphs -trigraphs -I . calculator.h
