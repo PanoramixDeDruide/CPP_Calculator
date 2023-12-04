a8aa5e94367947663567f218187907a90879ea11
Panoramix de Druïde <panoramix@xn--dru-1ma.de>
2023-12-04 11:19:24 +0100

    Updated the script thingie. Up next: a Makefile

--- process_numbers.sh
+++ process_numbers.sh
@@ -3,4 +3,3 @@
 if [[ ! -f processed_numbers.h ]]; then
 	cpp -P -Wno-trigraphs -I number_sources number_sources/numbers.h -o processed_numbers.h
 fi
-cpp -P -Wno-trigraphs -trigraphs -I . calculator.h
